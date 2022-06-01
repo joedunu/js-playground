b9250853f1fc827ccee3af9159d5b8426bbda8bd
Anuradha <anuradha.dunuhappawa@zendesk.com>
2022-06-01 22:40:32 +1000

    Add recursive adder logic

--- src/recursiveAdder.mjs
+++ src/recursiveAdder.mjs
@@ -1,16 +1,18 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
-function recursiveAdder(target) {
-  console.log(`Target number is: ${target}`);
+const recursiveAdder = (number, target, count = 1) => {
+  const multiplication = number * count++;
+  return multiplication <= target ? multiplication + recursiveAdder(number, target, count) : 0;
 }
 
 const main = async () => {
   const rl = readline.createInterface({ input, output });
 
-  const target = await rl.question('Enter your prefferd number to get the sum of multiplications: ');
+  const target = parseInt(await rl.question('Enter your prefferd number to get the sum of multiplications: '));
   rl.close();
-  recursiveAdder(target);
+  const answer = recursiveAdder(3, target) + recursiveAdder(5, target);
+  console.log(`Sum of multiplications is: ${answer}`);
 };
 
 main();
