33fe66fe7cd4984edd34ac19a27608df5f2b45be
Anuradha <anuradha.dunuhappawa@zendesk.com>
2022-06-01 22:46:32 +1000

    Add comments

--- src/recursiveAdder.mjs
+++ src/recursiveAdder.mjs
@@ -1,16 +1,28 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
+/**
+ * Function takes three arguments and passes back the sum of multiplications. 
+ * 
+ * First arguent passed in is multiplied by the thrid argument and if the answer is less than 
+ * the second argument it call the same function recursively until the answer is greater than the second arguent passed in.
+ * @param {number} number - Initial nuber passed in
+ * @param {number} target - Target for multiplication
+ * @param {number} count - counter for multiplication
+ * @returns sum of multiplications as an integer
+ */
 const recursiveAdder = (number, target, count = 1) => {
   const multiplication = number * count++;
   return multiplication <= target ? multiplication + recursiveAdder(number, target, count) : 0;
 }
 
 const main = async () => {
+  // Read input from the command line
   const rl = readline.createInterface({ input, output });
-
   const target = parseInt(await rl.question('Enter your prefferd number to get the sum of multiplications: '));
   rl.close();
+
+  // Call recursive adder function and print output to the command line
   const answer = recursiveAdder(3, target) + recursiveAdder(5, target);
   console.log(`Sum of multiplications is: ${answer}`);
 };
