4990d28a7afbfc3de759297fd81b6a413b6d30ba
Anuradha <anuradha.dunuhappawa@zendesk.com>
2022-06-28 17:58:54 +1000

    Optimise and fix bugs

--- src/recursiveAdder.mjs
+++ src/recursiveAdder.mjs
@@ -9,22 +9,48 @@ import { stdin as input, stdout as output } from 'node:process';
  * @param {number} factor - Initial nuber passed in
  * @param {number} target - Target for multiplication
  * @param {number} counter - counter for multiplication
+ * @param {array} multiplications - multiplications already added together
  * @returns sum of multiplications as an integer
  */
-const recursiveAdder = (factor, target, counter = 1) => {
+const recursiveAdder = (factor, target, counter = 1, multiplications) => {
   const multiplication = factor * counter;
-  return multiplication <= target ? multiplication + recursiveAdder(factor, target, ++counter) : 0;
+
+  if (multiplication <= target) {
+    const tmp = recursiveAdder(factor, target, ++counter, multiplications);
+    if (multiplications.find((mult) => multiplication === mult) === undefined) {
+      multiplications.push(multiplication);
+      return {
+        sum: multiplication + tmp.sum,
+        multiplications: tmp.multiplications
+      }
+    } else {
+      return tmp;
+    }
+  } else {
+    return { sum: 0, multiplications };
+  }
 }
 
 const main = async () => {
   // Read input from the command line
   const rl = readline.createInterface({ input, output });
   const target = parseInt(await rl.question('Enter your prefferd number to get the sum of multiplications: '));
+  let factorials = await rl.question('Enter the factorials to get the sum of multiplications sparated by spaces: ');
   rl.close();
 
+  factorials = factorials.split(' ').map(item => parseInt(item));
+
+  const multiplications = [];
+  console.log(`Factorials: ${factorials}, Target: ${target}`);
+
   // Call recursive adder function and print output to the command line
-  const answer = recursiveAdder(3, target) + recursiveAdder(5, target);
-  console.log(`Sum of multiplications is: ${answer}`);
+  let answer = factorials.map(item => recursiveAdder(item, target, 1, multiplications));
+  answer = answer.reduce((prev, cur) => {
+    console.log('PREV', prev.sum);
+    console.log('CUR', cur.sum);
+    return { sum: prev.sum + cur.sum, multiplications };
+  });
+  console.log('Sum of multiplications is: ', answer);
 };
 
 main();
